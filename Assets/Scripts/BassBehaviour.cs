e2d549b211cea38347df5ed55f46da60d4f36888
Andy Martin <andy@moonlightgames.net>
2020-08-08 15:27:22 -0500

    better error handling. ensuring bass is freed on close.

--- Assets/Scripts/BassBehaviour.cs
+++ Assets/Scripts/BassBehaviour.cs
@@ -9,37 +9,59 @@ public class BassBehaviour : MonoBehaviour
 {
     public Text status;
 
-    private void Start()
+	private bool isInitialized;
+
+	private string statusUpdateString;
+
+	private void Start()
     {
         status.text = "Initializing";
 
 		EncoderLoad();
 	}
 
-	private void EncoderLoad()
+    private void OnDestroy()
+    {
+        if(isInitialized)
+        {
+			Bass.BASS_Free();
+        }
+    }
+
+    private void Update()
+    {
+        if(!string.IsNullOrEmpty(statusUpdateString))
+        {
+			status.text = statusUpdateString;
+			statusUpdateString = null;
+        }
+    }
+
+    private void EncoderLoad()
 	{
 		try
         {
 			BassNet.Registration("zippo227@gmail.com", "test");
 
-			bool isInitialized = Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
+			isInitialized = Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
 			if (!isInitialized)
 			{
-				status.text = "Bass_Init error!";
+				statusUpdateString = "Bass_Init error!";
 			}
 			else
 			{
-				status.text = "Bass_Init success!";
+				statusUpdateString = "Bass_Init success!";
 			}
 
 			// init your recording device (we use the default device)
 			//if (!Bass.BASS_RecordInit(-1))
-			//	status.text = "Bass_RecordInit error!);
+            //{
+			//	statusUpdateString = "Bass_RecordInit error!";
+			//}
 		}
-		catch(Exception ex)
+		catch (Exception ex)
         {
-			Debug.LogError(ex.Message);
-			status.text = "Bass_Init error!";
+			statusUpdateString = $"Bass_Init error! {ex.Message}";
 		}
 	}
 
