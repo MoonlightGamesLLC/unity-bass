25474b69211b81bd6ed90c4eda42b2483b48ec7f
Andy Martin <andy@moonlightgames.net>
2020-08-08 14:53:49 -0500

    bass now loading in editor. should be ready for testing on iOS.

---
+++ Assets/Scripts/BassBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Un4seen.Bass;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BassBehaviour : MonoBehaviour
+{
+    public Text status;
+
+    private void Start()
+    {
+        status.text = "Initializing";
+
+		Encoder_Load();
+	}
+
+	private void Encoder_Load()
+	{
+		BassNet.Registration("zippo227@gmail.com", "test");
+
+		bool isInitialized = Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
+		if (!isInitialized)
+        {
+			status.text = "Bass_Init error!";
+		}
+        else
+        {
+			status.text = "Bass_Init success!";
+		}
+
+		// init your recording device (we use the default device)
+		//if (!Bass.BASS_RecordInit(-1))
+		//	status.text = "Bass_RecordInit error!);
+	}
+
+}
